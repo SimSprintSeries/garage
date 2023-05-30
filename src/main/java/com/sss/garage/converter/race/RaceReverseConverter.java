cbb81f475e285da5db4343a2facaf2cf82301a57
Raf <rafal.kowalski3@edu.uekat.pl>
2023-05-30 15:42:49 +0200

    add race endpoints

--- src/main/java/com/sss/garage/converter/race/RaceReverseConverter.java
+++ src/main/java/com/sss/garage/converter/race/RaceReverseConverter.java
@@ -4,6 +4,7 @@ import com.sss.garage.converter.BaseConverter;
 import com.sss.garage.converter.event.EventReverseConverter;
 import com.sss.garage.data.race.RaceData;
 import com.sss.garage.model.race.Race;
+import com.sss.garage.model.split.Split;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
@@ -19,6 +20,7 @@ public class RaceReverseConverter extends BaseConverter implements Converter<Rac
         eventReverseConverter.convert(source, target);
 
         target.setId(source.getId());
+        target.setSplit(getConversionService().convert(source.getSplit(), Split.class));
 
         return target;
     }
