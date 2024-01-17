121a5204a4735fd6fb6c901aa83cac1e493250ae
Raf <rafal.kowalski3@edu.uekat.pl>
2024-01-17 21:32:49 +0100

    add F1 and ACC rules

---
+++ src/main/java/com/sss/garage/controller/rules/RulesController.java
@@ -0,0 +1,29 @@
+package com.sss.garage.controller.rules;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import static com.sss.garage.constants.WebConstants.RULES_ENDPOINT;
+
+@Controller
+@RequestMapping(RULES_ENDPOINT)
+@Tag(name = "Sss Rules")
+public class RulesController {
+    @GetMapping("/f1")
+    @Operation(operationId = "getF1Rules", summary = "Get rules for F1 events")
+    @ResponseStatus(HttpStatus.OK)
+    public String getF1Rules() {
+        return "f1rules";
+    }
+
+    @GetMapping("/acc")
+    @Operation(operationId = "getACCRules", summary = "Get rules for ACC events")
+    @ResponseStatus(HttpStatus.OK)
+    public String getACCRules() {
+        return "accrules";
+    }
+}
