ac724b860f6634ad99587b777b6c2e9d6221b510
Ryszard Lipton <wa.drzewiecki@gmail.com>
2024-08-20 00:00:54 +0200

    Try new classification ordering

--- src/main/java/com/sss/garage/service/classification/impl/SssClassificationService.java
+++ src/main/java/com/sss/garage/service/classification/impl/SssClassificationService.java
@@ -3,8 +3,6 @@ package com.sss.garage.service.classification.impl;
 import com.sss.garage.model.classification.Classification;
 import com.sss.garage.model.driver.Driver;
 import com.sss.garage.model.driver.DriverRepository;
-import com.sss.garage.model.event.Event;
-import com.sss.garage.model.event.EventRepository;
 import com.sss.garage.model.league.League;
 import com.sss.garage.model.raceresult.RaceResultRepository;
 import com.sss.garage.model.team.Team;
@@ -46,24 +44,58 @@ public class SssClassificationService implements ClassificationService {
             classification.setPoints(raceResultRepository.findPointsByDriverAndLeague(driver, league));
             classifications.add(classification);
         }
-
-        List<Classification> duplicates = findDuplicates(classifications);
-
-        for (int j = 1; j < 11; j++) {
-            if (!duplicates.isEmpty()) {
-                List<Classification> filtered = new ArrayList<>();
-                for (Classification classification : duplicates) {
-                    classification.setPosition(j);
-                    classification.setPositionCount(raceResultRepository
-                            .countFinishPositionByDriverAndLeague(classification.getDriver(), classification.getLeague(), j));
-                    filtered.add(classification);
+        
+        final List<Map.Entry<Integer, List<Classification>>> sortedClassifications =
+            classifications.stream()
+                .collect(Collectors.groupingBy(Classification::getPoints))
+                .entrySet().stream()
+                .sorted(Map.Entry.comparingByKey()).toList();
+        
+        int position = 1;
+        for(Map.Entry<Integer, List<Classification>> entry : sortedClassifications) {
+            if(entry.getValue().size() == 1) {
+                entry.getValue().get(0).setPosition(position++);
+            }
+            else { // For sure more than 1, never 0
+                while (entry.getValue().size() > 1) {
+                    Classification winner = findWinnerInDuplicates(entry.getValue());
+                    winner.setPosition(position++);
+                    entry.getValue().remove(winner);
                 }
-                duplicates = findDuplicates(filtered);
+                entry.getValue().get(0).setPosition(position++);
             }
         }
-
-        sortClassification(classifications);
+        
         return new PageImpl<>(classifications, pageable, classifications.size());
+    }
+    
+    private Classification findWinnerInDuplicates(final List<Classification> classifications) {
+        if(classifications.size() == 1) {
+            return classifications.get(0);
+        }
+        for(int checkingPosition = 1; checkingPosition <= 20; checkingPosition++) {
+            for (Classification classification : classifications) {
+                Integer thisPositionCount = raceResultRepository.countFinishPositionByDriverAndLeague(
+                    classification.getDriver(), classification.getLeague(), checkingPosition);
+                if (thisPositionCount == null) {
+                    thisPositionCount = 0;
+                }
+                
+                classification.setPositionCount(classification.getPositionCount() + thisPositionCount);// The sum is not really necessary, but whoever has the highest wins
+            }
+            
+            classifications.sort(Comparator.comparing(Classification::getPositionCount).reversed());
+            
+            if(classifications.get(0).getPositionCount() > classifications.get(1).getPositionCount()) {
+                for(int i = 1; i < classifications.size(); i++) {
+                    classifications.get(i).setPositionCount(0);// clear for next round
+                }
+                return classifications.get(0);
+            }
+        }
+        
+        // Almost impossible, drivers have exactly the same number of positions, return first one
+        return classifications.get(0);
     }
 
     private Page<Classification> setClassificationForTeams(final League league, final Pageable pageable) {
@@ -103,7 +135,7 @@ public class SssClassificationService implements ClassificationService {
 
     private List<Classification> findDuplicates(final List<Classification> classifications) {
         return classifications.stream()
-                .collect(Collectors.groupingBy(i -> Arrays.asList(i.getPoints(), i.getPosition(), i.getPositionCount())))
+                .collect(Collectors.groupingBy(i -> List.of(i.getPoints(), i.getPosition(), i.getPositionCount())))
                 .values().stream()
                 .filter(classificationList -> classificationList.size() > 1).flatMap(List::stream).toList();
     }
