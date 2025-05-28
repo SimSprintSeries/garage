a838af618e823ce8db6671d6dff6119f2693142c
Ryszard Lipton <wa.drzewiecki@gmail.com>
2025-05-28 16:09:04 +0200

    Revert "test 3"
    This reverts commit f1c34bcfcd3394f73700bf17c048f5edee1ebd2e.

--- src/main/java/com/sss/garage/service/discord/converter/DiscordRoleConverter.java
+++ src/main/java/com/sss/garage/service/discord/converter/DiscordRoleConverter.java
@@ -4,20 +4,15 @@ import com.sss.garage.model.role.DiscordRole;
 
 import net.dv8tion.jda.api.entities.Role;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DiscordRoleConverter implements Converter<Role, DiscordRole> {
 
-    Logger logger = LoggerFactory.getLogger(DiscordRoleConverter.class);
-
     @Override
     public DiscordRole convert(final Role source) {
         DiscordRole target = new DiscordRole();
-        logger.info(source.getId());
         target.setId("ROLE_" + source.getId());
         target.setCode(source.getName());
         return target;
